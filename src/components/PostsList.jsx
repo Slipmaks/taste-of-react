c733e058e1219c1a7d2e3c80ecf988c900c4d5c6
Slipmaks <slipmaks@gmail.com>
2023-03-03 17:14:51 +0200

    init

---
+++ src/components/PostsList.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+import Post from "./Post";
+import NewPost from "./NewPost";
+import Modal from "./Modal";
+import classes from "./PostsList.module.css";
+function PostList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+  return (
+    <>
+      {modalIsVisible ? (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      ) : null}
+
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Test author" body="test body" />
+      </ul>
+    </>
+  );
+}
+export default PostList;
